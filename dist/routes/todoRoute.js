15fc3878e71f49f65a7afe9f27c4cb819d13da19
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-21 14:40:54 +0530

    update http method

--- dist/routes/todoRoute.js
+++ dist/routes/todoRoute.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const checkToken_1 = require("../middleware/checkToken");
 const TodoController_1 = require("../controllers/TodoController");
-router.post("/update", checkToken_1.protect, TodoController_1.updatetodo);
+router.put("/update", checkToken_1.protect, TodoController_1.updatetodo);
 router.get("/getall/:id", TodoController_1.getAlltodo);
 router.get("/get/:id", TodoController_1.getSpecifictodo);
 router.post("/add", checkToken_1.protect, TodoController_1.addtodo);
