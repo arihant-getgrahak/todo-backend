280cfe41b89ba4142a408dcae294805977fa637d
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-21 15:14:58 +0530

    Update generateToken

--- dist/utils/generateToken.js
+++ dist/utils/generateToken.js
@@ -1,10 +1,8 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateToken = void 0;
 const jwt = require("jsonwebtoken");
 const generateToken = (id, email) => {
     return jwt.sign({ id, email }, process.env.JWT_SECRET, {
         expiresIn: "30d",
     });
 };
-exports.generateToken = generateToken;
+module.exports = generateToken;
